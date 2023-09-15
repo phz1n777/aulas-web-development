4a2daff235ba5d62476bd92823e459a583e62803
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:47:30 -0300

    Adicionando variaveis no JS

--- aula.js
+++ aula.js
@@ -1,1 +1,10 @@
 alert("Olá mundo!");
+
+//Adicionando mensagm na tela
+document.write('Olá mundo!');
+
+console.log('Olá mundo')
+
+//Criando uma variavel
+let mensagm = "Olá mundo"
+console.log("Mensagem de variavel: ", mensagem);
