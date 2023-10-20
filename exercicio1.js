8706cd76fe44760b04ca5801d14c98ed0e078dc5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:29:51 -0300

    exercicio1

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+// Escreva um loop (for ou while) que exiba a frase abaixo no console;
+// -Substitua o "X" pela informação correta;
+// -"X" deve começar em 1 na 1ª exibição da frase e em 10 na ultima;
+// -Exiba a frase 10x no console.
+
+// "Esta é a Xª vez que esta frase é exibida no console"
+
+for(let i = 0; i < 11; i++){
+    console.log(`"Está é a ${i} vez que esta frase é exibida no console"`)
+}
